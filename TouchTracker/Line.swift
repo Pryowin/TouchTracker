11d22c23e802285a276c0eac2228fd7ea40b2932
David Burke <david.burke@fundingcircle.com>
2017-07-20 19:49:09 -0700

    Single line drawing

---
+++ TouchTracker/Line.swift
@@ -0,0 +1,15 @@
+//
+//  Line.swift
+//  TouchTracker
+//
+//  Created by David Burke on 7/20/17.
+//  Copyright © 2017 amberfire. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+struct Line {
+    var begin = CGPoint.zero
+    var end = CGPoint.zero
+}
