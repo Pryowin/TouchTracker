8f3fff733efe0c6bed1fb8e3b888d9eadc84869e
David Burke <david.burke@fundingcircle.com>
2017-07-21 18:11:13 -0700

    Lines can now be deleted

--- TouchTracker/DrawView.swift
+++ TouchTracker/DrawView.swift
@@ -9,8 +9,21 @@
 import UIKit
 
 class DrawView: UIView {
+    
+    //MARK: - Variable definition
+    
     var currentLines = [NSValue:Line]()
     var finishedLines = [Line]()
+    var selectedLineIndex: Int? {
+        didSet {
+            if selectedLineIndex == nil {
+                let menu = UIMenuController.shared
+                menu.setMenuVisible(false, animated: true)
+            }
+        }
+    }
+    
+    // MARK: - Inspectable Items
     
     @IBInspectable var finishedLineColor: UIColor = UIColor.black {
         didSet{
@@ -28,6 +41,52 @@ class DrawView: UIView {
         }
     }
     
+    // MARK: - Functions
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        let doubleTapRecognizer = UITapGestureRecognizer(target: self, action: #selector(DrawView.doubleTap(_:)) )
+        doubleTapRecognizer.numberOfTapsRequired = 2
+        doubleTapRecognizer.delaysTouchesBegan = true
+        addGestureRecognizer(doubleTapRecognizer)
+        
+        let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(DrawView.tap(_:)))
+        tapRecognizer.delaysTouchesBegan = true
+        tapRecognizer.require(toFail: doubleTapRecognizer)
+        addGestureRecognizer(tapRecognizer)
+
+        
+    }
+    func doubleTap(_ gestureRecognizer: UIGestureRecognizer) {
+        print("Recognized a double tap")
+        selectedLineIndex = nil
+        currentLines.removeAll()
+        finishedLines.removeAll()
+        setNeedsDisplay()
+    }
+    func tap(_ gestureRecognizer: UIGestureRecognizer) {
+        print("Recognized a tap")
+        
+        let point = gestureRecognizer.location(in: self)
+        selectedLineIndex = indexOfLine(at: point)
+        
+        let menu = UIMenuController.shared
+        if (selectedLineIndex != nil) {
+            becomeFirstResponder()
+            let deleteItem = UIMenuItem(title: "Delete", action: #selector(DrawView.deleteLine(_:)))
+            menu.menuItems = [deleteItem]
+            let targetRect = CGRect(x: point.x, y: point.y, width: 2, height: 2)
+            menu.setTargetRect(targetRect, in: self)
+            menu.setMenuVisible(true, animated: true)
+        } else {
+            menu.setMenuVisible(false, animated: true)
+        }
+        
+        setNeedsDisplay()
+    }
+    
+    
     func stroke(_ line: Line) {
         let path = UIBezierPath()
         path.lineWidth = lineWidth
@@ -36,6 +95,32 @@ class DrawView: UIView {
         path.addLine(to: line.end)
         path.stroke()
     }
+    
+    func indexOfLine(at point: CGPoint) -> Int? {
+        for (index, line) in finishedLines.enumerated() {
+            let begin = line.begin
+            let end = line.end
+            for t in stride(from: CGFloat(0), to: 1, by: 0.05) {
+                let x = begin.x + ((end.x - begin.x) * t)
+                let y = begin.y + ((end.y - begin.y) * t)
+                if hypot(x - point.x, y - point.y) < 20.0  {
+                    return index
+                }
+            }
+        }
+        return nil
+    }
+    
+    func deleteLine(_ sender: UIMenuController) {
+        if let index = selectedLineIndex {
+            finishedLines.remove(at: index)
+            selectedLineIndex = nil
+            setNeedsDisplay()
+        }
+    }
+    
+    
+    // MARK: - Overrides of touch and draw
     
     override func draw(_ rect: CGRect) {
         
@@ -49,9 +134,15 @@ class DrawView: UIView {
         for (_, line) in currentLines {
                 stroke(line)
         }
+        if let index = selectedLineIndex {
+            UIColor.green.setStroke()
+            let selectedLine = finishedLines[index]
+            stroke(selectedLine)
+        }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print(#function)
         
         for touch in touches {
             let location = touch.location(in: self)
@@ -71,6 +162,7 @@ class DrawView: UIView {
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print(#function)
         
         for touch in touches {
             let key = NSValue(nonretainedObject: touch)
@@ -85,8 +177,13 @@ class DrawView: UIView {
     }
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print(#function)
         currentLines.removeAll()
         setNeedsDisplay()
+    }
+    
+    override var canBecomeFirstResponder: Bool {
+        return true
     }
     
 }
